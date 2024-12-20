46c373bf1637c6c3608fa93ff0be9345e16031ee
janaki799 <sangatijanaki@gmail.com>
2024-12-20 12:23:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const allowedOrigins = [
-    'https://frontend-8ecmtkquq-janaki799s-projects.vercel.app',
+    'https://my-frontenf-server.onrender.com',
     'http://localhost:3000',
     'http://localhost:3001'
 ];
