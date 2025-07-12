8b542860548dc3e901df6113f1178e28d3a8de8d
unknown <sangatijanaki@gmail.com>
2025-07-12 17:06:11 +0530

    pushing the backend files

--- models/report.js
+++ models/report.js
@@ -7,7 +7,13 @@ const reportSchema = new mongoose.Schema({
   incidentCategory: { type: String, required: true },
   incidentType: { type: String, required: true },
   description: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
+  localDate: { type: String },
+  status: {
+        type: String,
+        enum: ['Pending', 'In Progress', 'Resolved'],
+        default: 'Pending'
+    },
 });
 
 // Report Model
